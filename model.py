d7e3c723e33ca90296076a7e7a6d34e4bc5875ff
zeichen <zeichen@umich.edu>
2022-03-28 17:25:48 -0400

    modified base model to match with base paper more closely

--- model.py
+++ model.py
@@ -2,4 +2,5 @@ import torch
 from transformers import BertForSequenceClassification
 import transformers
 
-model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
+# changed from bert-base-uncased to match base paper more closely 
+model = BertForSequenceClassification.from_pretrained("microsoft/deberta-base")
