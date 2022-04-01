226db37cd267e29e17b968af69641e044a0f6609
Jensen Hwa <30474284+jensenhwa@users.noreply.github.com>
2022-04-01 17:58:54 -0400

    Disable model weights regardless of model type

--- model.py
+++ model.py
@@ -4,5 +4,6 @@ from transformers import DistilBertForSequenceClassification
 # model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
 model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=13)
 
-for param in model.distilbert.parameters():
-    param.requires_grad = False
+# for param in model.base_model.parameters():
+#     param.requires_grad = False
+model.dropout.train()
