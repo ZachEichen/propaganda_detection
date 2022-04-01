22da27386c3ec198634a7bec2a7ec816a285d65c
Jensen Hwa <30474284+jensenhwa@users.noreply.github.com>
2022-04-01 15:44:39 -0400

    Code cleanup

--- dataset.py
+++ dataset.py
@@ -1,13 +1,14 @@
+import csv
+from pathlib import Path
+
+import pandas as pd
 import torch
-from torch import nn
 from torch.utils.data import Dataset
 from tqdm.auto import tqdm
-from pathlib import Path
-import csv
-import pandas as pd 
-from collections import OrderedDict
+from transformers import DistilBertTokenizerFast
 
-from transformers import BertTokenizerFast
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
 
 class ProppyDataset(Dataset):
     """Dataset for Proppy dataset (binary classification)"""
@@ -17,7 +18,6 @@ class ProppyDataset(Dataset):
         if not filepath.is_file():
             raise Exception("Invalid filepath to tsv file")
         self.data = []
-        tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased')
         with open(filepath, newline='') as tsvfile:
             reader = csv.reader(tsvfile, dialect=csv.excel_tab)
             for row in tqdm(reader):
@@ -47,7 +47,6 @@ class FallacyDataset(Dataset):
         super().__init__()
         if not filepath.is_file():
             raise Exception("Invalid filepath to csv file")
-        tokenizer = BertTokenizerFast.from_pretrained('bert-base-uncased',num_labels = 13)
         data_df = pd.read_csv(filepath)
         if not ("logical_fallacies" in data_df.columns and "source_article" in data_df.columns):
             raise Exception("could not find correct columns in input")
@@ -61,19 +60,20 @@ class FallacyDataset(Dataset):
                 .to_dict(orient="records")
             )
         self.encoding = tokenizer([x['text'] for x in self.data], return_tensors='pt', padding=True, truncation=True)
+
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         item = {key: torch.tensor(val[idx]) for key, val in self.encoding.items()}
-        # item['label'] = self.data[idx]['fallacy']
-        # labels cannot be strings, so modifying like below
+        item['text'] = self.data[idx]['text']
         item['label'] = self.fallacy_dict[self.data[idx]['fallacy']]
         return item    
 
     @property
     def label_to_fallacy(self):
         return self.fallacy_list
+
     @property
     def fallacy_to_label(self):
         return self.fallacy_dict
--- model.py
+++ model.py
@@ -1,11 +1,8 @@
-import torch 
-# from transformers import DistilBertTokenizer, DistilBertModel
-import transformers
+import torch
 from transformers import DistilBertForSequenceClassification
 
-model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels = 13)
-
-#model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+# model = BertForSequenceClassification.from_pretrained('bert-base-uncased')
+model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased", num_labels=13)
 
 for param in model.distilbert.parameters():
     param.requires_grad = False
